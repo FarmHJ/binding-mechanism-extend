a7b8b1fcd958193368fe9eddfd1a253ecbccbe4f
FarmHJ <huijia-farm_95@hotmail.com>
2023-02-07 18:58:59 +0000

    Check AP models are simulated correctly

--- scripts/model_sim.py
+++ scripts/model_sim.py
@@ -1,10 +1,13 @@
+import matplotlib.pyplot as plt
 import myokit
+import numpy as np
 
 import modelling
 
 # Define protocol
 pulse_time = 1000
-protocol = myokit.pacing.blocktrain(pulse_time, 0.5, offset=10)
+protocol_offset = 10
+protocol = myokit.pacing.blocktrain(pulse_time, 0.5, offset=protocol_offset)
 
 #
 # Grandi (2010) model
@@ -18,6 +21,15 @@ protocol = myokit.pacing.blocktrain(pulse_time, 0.5, offset=10)
 abs_tol = 1e-7
 rel_tol = 1e-8
 # log = AP_model.model_simulation(1000, abs_tol=abs_tol, rel_tol=rel_tol)
+
+# AP_peak, _ = AP_model.extract_peak(log, 'membrane_potential.V_m')
+# APD90 = AP_model.APD90(log['membrane_potential.V_m'], protocol_offset, timestep=0.1)
+# resting_Vm = min(log['membrane_potential.V_m'][500 / 0.1:])
+# print('resting membrane potential: ', resting_Vm)
+# print('AP amplitude:', AP_peak[0] - resting_Vm)
+# print('APD90: ', APD90)
+# print('maximal upstroke velocity: ', 
+#       max(log['dot(membrane_potential.V_m)']))
 
 # # Plot output
 # fig = modelling.figures.FigureStructure(figsize=(5, 3), gridspec=(2, 1))
@@ -62,17 +74,31 @@ rel_tol = 1e-8
 #
 # ten Tusscher (2006) model
 #
+# Define protocol
+timestep = 0.1
+protocol_offset = 100
+protocol = myokit.pacing.blocktrain(pulse_time, 0.5, offset=protocol_offset)
 # Set up AP model
 APmodel = '../math_model/AP_model/TTP-2006.mmt'
 APmodel, _, x = myokit.load(APmodel)
 AP_model = modelling.Simulation(APmodel)
 AP_model.protocol = protocol
 
-log = AP_model.model_simulation(1000, abs_tol=abs_tol, rel_tol=rel_tol)
+log = AP_model.model_simulation(1000, timestep=timestep, abs_tol=abs_tol, rel_tol=rel_tol)
 
-print(min(log['membrane.V'][500:]), max(log['membrane.V']))
+# Action potential measures
+AP_peak, _ = AP_model.extract_peak(log, 'membrane.V')
+APD90 = AP_model.APD90(log['membrane.V'], protocol_offset, timestep=timestep)
+resting_Vm = min(log['membrane.V'][500 * 10:])
+print('###############')
+print('resting membrane potential: ', resting_Vm)
+print('AP amplitude:', AP_peak[0] - resting_Vm)
+print('APD90: ', APD90)
+print('maximal upstroke velocity: ', 
+      max(log['dot(membrane.V)']))
+
 # Plot TTP's action potential
-fig = modelling.figures.FigureStructure(figsize=(5, 3), gridspec=(2, 1))
+fig = modelling.figures.FigureStructure(figsize=(8, 3), gridspec=(2, 1))
 plot = modelling.figures.FigurePlot()
 
 plot.add_single(fig.axs[0][0], log, 'membrane.i_Stim')
@@ -84,3 +110,48 @@ fig.sharey(['Current\n(A/F)', 'Voltage\n(mV)'])
 
 fig_dir = '../figures/basic_sim/'
 fig.savefig(fig_dir + "ttp_sim.pdf")
+
+# Calcium dynamics measures
+Ca_SS = log['calcium_dynamics.Ca_ss']
+CaSS_peak_amplitude = max(Ca_SS) - min(Ca_SS)
+print('CaSS peak amplitude: ', CaSS_peak_amplitude)
+Ca_i = log['calcium_dynamics.Ca_i']
+Cai_peak_amplitude = max(Ca_i) - min(Ca_i)
+print('Cai peak amplitude: ', Cai_peak_amplitude)
+# CaSS_time2peak = (np.argmax(Ca_SS) - np.argmax(log['dot(calcium_dynamics.Ca_ss)'])) * timestep
+CaSS_time2peak = np.sum(log['dot(calcium_dynamics.Ca_ss)'] > 0.01 * 1) * timestep
+print('CaSS time to peak: ', CaSS_time2peak)
+# Cai_time2peak = (np.argmax(Ca_i) - np.argmax(log['dot(calcium_dynamics.Ca_i)'])) * timestep
+Cai_time2peak = np.sum(log['dot(calcium_dynamics.Ca_i)'] > 1e-6 * 1) * timestep
+print('Cai time to peak: ', Cai_time2peak)
+CaSS_duration = np.sum((Ca_SS - min(Ca_SS)) > 0.1 * 1) * timestep
+print('CaSS time to peak: ', CaSS_duration)
+# Cai_time2peak = np.sum(log['calcium_dynamics.Ca_i'] > 1e-6 * 1) * timestep
+# print('Cai time to peak: ', Cai_time2peak)
+
+# Check the computation of time to peak
+plt.figure()
+plt.plot(log['calcium_dynamics.Ca_ss'])
+# plt.plot((log['dot(calcium_dynamics.Ca_i)'] > 1e-5) * max(Ca_i), '--')
+plt.plot((Ca_SS - min(Ca_SS)) > 0.1 * 1 * max(Ca_SS), '--')
+# plt.xlim(80 * 10, 220 * 10)
+fig_dir = '../figures/basic_sim/'
+plt.savefig(fig_dir + 'test.pdf')
+
+# Plot output
+fig = modelling.figures.FigureStructure(figsize=(5, 2), gridspec=(1, 3),
+                                        wspace=0.5)
+
+current_list = ['membrane.V', 'calcium_dynamics.Ca_i',
+                'calcium_dynamics.Ca_ss']
+current_name = ['Vm', 'Cai', 'CaSS']
+
+for i in range(len(current_list)):
+    plot.add_single(fig.axs[0][i], log, current_list[i])
+    fig.axs[0][i].spines[['right', 'top']].set_visible(False)
+    fig.axs[0][i].set_ylabel(current_name[i])
+
+fig.sharex(['Time (ms)'] * 3, [(0, 600), (0, 800), (0, 600)])
+
+fig_dir = '../figures/basic_sim/'
+fig.savefig(fig_dir + "ttp_sim_currents.pdf")
