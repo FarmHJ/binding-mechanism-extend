2a8c442da4cd1d5388a3748a9e4238d76eb28712
FarmHJ <hui.farm@cs.ox.ac.uk>
2024-06-17 14:53:32 +0100

    Terminate simulation when EAD is detected

--- modelling/model_comparison.py
+++ modelling/model_comparison.py
@@ -1,7 +1,12 @@
 import numpy as np
 import pandas as pd
+import time
+
+import matplotlib.pyplot as plt
+import os
 
 import modelling
+# TODO: raise warning if Hill curve not formed
 
 
 class ModelComparison(object):
@@ -22,7 +27,7 @@ class ModelComparison(object):
         if IKr_simulator is not None and not \
                 isinstance(IKr_simulator, modelling.ModelSimController):
             raise TypeError(
-                'The model has to be an instance of a '
+                'The model has to be an instance of a ''Lei'
                 'modelling.ModelSimController.')
         self.IKr_sim = IKr_simulator
 
@@ -33,17 +38,13 @@ class ModelComparison(object):
     def prepare_inputs(self, input, skip_ikr=False):
         self.id_index = input['id']
         param_values = input['param_values']
-        self._set_parameters(param_values)
+        self.param_df = param_values
 
         if skip_ikr:
-            drug_conc = input['drug_conc_Hill'].values[0]
-            self.drug_conc_ikr = drug_conc[~np.isnan(drug_conc)]
-            self.Hill_coef = input['Hill_curve'].values[0]
-
-    def _set_parameters(self, param_df):
-        if 'Cmax' in param_df.columns:
-            param_df = param_df.drop(columns=['Cmax'])
-        self.param_df = param_df
+            drug_conc = input['drug_conc_Hill']
+            self.drug_conc_ikr = np.array(drug_conc)[~np.isnan(drug_conc)]
+            self.Hill_coef = [input['Hill_curve']['Hill_coef'],
+                              input['Hill_curve']['IC50']]
 
     def normalise_drug_conc(self):
         '''
@@ -102,8 +103,8 @@ class ModelComparison(object):
                                for i in peaks_norm]
             counter += 1
 
-        if counter == max_counter:
-            return 'Hill curve did not form.', drug_conc, peaks_norm
+        # if counter == max_counter:
+        #     return 'Hill curve did not form.', drug_conc, peaks_norm
 
         # Make sure there are enough data points for the tail of Hill curve
         data_pt_checker = [True if i < lower_thres else False
@@ -119,8 +120,8 @@ class ModelComparison(object):
                                for i in peaks_norm]
             counter += 1
 
-        if counter == max_counter:
-            return 'Hill curve did not form.', drug_conc, peaks_norm
+        # if counter == max_counter:
+        #     return 'Hill curve did not form.', drug_conc, peaks_norm
 
         # Fit Hill curve
         optimiser = modelling.HillModelOpt(self.Hill_model)
@@ -136,14 +137,21 @@ class ModelComparison(object):
 
     def _get_apd90(self, drug_conc, save_signal=2):
 
+        # start = time.time()
         self.APsim.set_conc(drug_conc)
         log = self.APsim.simulate(
             save_signal=save_signal,
             log_var=[self.APsim.time_key, self.APsim.Vm_key])
 
-        apd90_SD = self.APsim.APD90(log)
+        plt.figure()
+        plt.plot(log.time(), log[self.APsim.Vm_key, 0], 'k-', label='SD')
+        plt.plot(np.array(log.time()) + self.APsim._cycle_length,
+                 log[self.APsim.Vm_key, 1], 'k-')
 
-        # Run simulation for conductance model
+        apd90_SD = self.APsim.APD90(log)
+        # print('Compute APD90 for SD model: ', time.time() - start)
+
+        # start = time.time()
         reduction_scale = self.Hill_model.simulate(
             self.Hill_coef, drug_conc)
         self.APsim.set_conc(0)
@@ -164,8 +172,6 @@ class ModelComparison(object):
         self.APsim.reset_parameters()
         self.APsim.set_parameters(self.param_df)
 
-        APD_trapping = []
-        APD_conductance = []
         if drug_conc is None:
             if hasattr(self, 'drug_conc_ikr'):
                 drug_conc = 10**np.linspace(np.log10(self.drug_conc_ikr[1]),
@@ -175,6 +181,8 @@ class ModelComparison(object):
                 drug_conc = 10**np.linspace(-1, 5, data_points)
         drug_conc = list(drug_conc)
 
+        APD_trapping = []
+        APD_conductance = []
         for i in range(len(drug_conc)):
             apd90_SD, apd90_CS = self._get_apd90(drug_conc[i],
                                                  save_signal=save_signal)
@@ -189,10 +197,10 @@ class ModelComparison(object):
         if EAD:
             checker_trapping = [np.isnan(i) for i in APD_trapping]
             checker_conductance = [np.isnan(i) for i in APD_conductance]
-            checker_count = min(sum(checker_trapping),
+            checker_count = max(sum(checker_trapping),
                                 sum(checker_conductance))
             counter = 0
-            while checker_count < 2 and counter < 10:
+            while checker_count < 1 and counter < 10:
                 drug_conc = drug_conc + [max(drug_conc) * np.sqrt(10)]
                 apd90_SD, apd90_CS = self._get_apd90(drug_conc[-1],
                                                      save_signal=save_signal)
@@ -207,7 +215,7 @@ class ModelComparison(object):
 
                 checker_trapping = [np.isnan(i) for i in APD_trapping]
                 checker_conductance = [np.isnan(i) for i in APD_conductance]
-                checker_count = min(sum(checker_trapping),
+                checker_count = max(sum(checker_trapping),
                                     sum(checker_conductance))
 
                 counter += 1
@@ -229,12 +237,16 @@ class ModelComparison(object):
                 square_sum += (self.APD_trapping[i] -
                                self.APD_conductance[i])**2
                 count += 1
+            # else:
+            #     break
 
         if count != 0:
             self.RMSError = square_sum / count
             self.RMSError = np.sqrt(self.RMSError)
+            self.Error = self.RMSError
         else:
             self.RMSError = float('nan')
+            self.Error = self.RMSError
 
     def ME(self):
 
@@ -248,15 +260,48 @@ class ModelComparison(object):
 
         if count != 0:
             self.MAError = diff_sum / count
+            self.Error = self.MAError
         else:
             self.MAError = float('nan')
+            self.Error = self.MAError
+
+    def ME_ratio(self):
+
+        ratio_sum = 0
+        count = 0
+        for i in range(len(self.APD_trapping)):
+            if not np.isnan(self.APD_trapping[i]) and not \
+                    np.isnan(self.APD_conductance[i]):
+                ratio_sum += (self.APD_trapping[i] - self.APD_conductance[i]) \
+                    / self.APD_conductance[i]
+                count += 1
+
+        if count != 0:
+            self.Error = ratio_sum / count
+        else:
+            self.Error = float('nan')
+
+    def RMSE_ratio(self):
+
+        ratio_sum = 0
+        count = 0
+        for i in range(len(self.APD_trapping)):
+            if not np.isnan(self.APD_trapping[i]) and not \
+                    np.isnan(self.APD_conductance[i]):
+                ratio_sum += (self.APD_trapping[i] - self.APD_conductance[i])**2 \
+                    / self.APD_conductance[i]**2
+                count += 1
+
+        if count != 0:
+            self.Error = ratio_sum / count
+            self.Error = np.sqrt(self.Error)
+        else:
+            self.Error = float('nan')
 
     def process_data(self, save_orig_halfmax=False):
 
-        conc_ikr_ind = ['conc_' + str(i) for i, _ in
-                        enumerate(self.drug_conc_ikr)]
-        conc_AP_ind = ['conc_' + str(i) for i, _ in
-                       enumerate(self.drug_conc_AP)]
+        conc_ikr_ind = [f'conc_{i}' for i in range(len(self.drug_conc_ikr))]
+        conc_AP_ind = [f'conc_{i}' for i in range(len(self.drug_conc_AP))]
 
         index_dict = {'drug_conc_ikr': conc_ikr_ind,
                       'Hill_curve': ['Hill_coef', 'IC50'],
@@ -275,7 +320,7 @@ class ModelComparison(object):
             self.param_df['halfmax'] = self.orig_halfmax
 
         data_list = list(self.drug_conc_ikr) + list(self.Hill_coef) + \
-            list(self.param_df.values[0]) + list(self.drug_conc_AP) + \
+            list(self.param_df.values()) + list(self.drug_conc_AP) + \
             self.APD_trapping + self.APD_conductance + \
             [self.RMSError] + [self.MAError]
         if hasattr(self, 'peaks_norm'):
