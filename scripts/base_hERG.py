8af8ad8d8030a11634fe7b43c2de20babe46b754
FarmHJ <hui.farm@cs.ox.ac.uk>
2024-06-18 10:49:41 +0100

    Figure comparing Li and Lei models

--- scripts/base_hERG.py
+++ scripts/base_hERG.py
@@ -8,22 +8,27 @@ data_dir = os.path.join(modelling.RESULT_DIR, 'background')
 if not os.path.isdir(data_dir):
     os.makedirs(data_dir)
 
-# Load AP models
-APsim = modelling.ModelSimController('ORd-Li')
+model_list = modelling.model_naming.APmodel_list[:-1]
+for APmodel in model_list:
+    # Load AP models
+    APsim = modelling.ModelSimController(APmodel)
+    if APmodel != 'ORd-Li':
+        APsim.set_ikr_rescale_method('AP_duration')
 
-# Simulate AP for AP clamp protocol
-APclamp = APsim.simulate(log_var=[APsim.time_key, APsim.Vm_key])
-APclamp.save_csv(os.path.join(data_dir, 'APclamp.csv'))
+    # Simulate AP for AP clamp protocol
+    APclamp = APsim.simulate(log_var=[APsim.time_key, APsim.Vm_key])
+    APclamp.save_csv(os.path.join(data_dir, f'APclamp_{APmodel}.csv'))
 
-# Set up AP clamp protocol
-times = APclamp[APsim.time_key]
-voltages = APclamp[APsim.Vm_key]
+    # Set up AP clamp protocol
+    times = APclamp[APsim.time_key]
+    voltages = APclamp[APsim.Vm_key]
 
-# Load IKr model
-model_list = ['Li', 'Lei']
-for model_name in model_list:
-    IKr_sim = modelling.ModelSimController(model_name)
-    IKr_sim.set_protocol(times, voltages)
-    log = IKr_sim.simulate(log_var='all')
+    # Load IKr model
+    model_list = ['Li', 'Lei']
+    for model_name in model_list:
+        IKr_sim = modelling.ModelSimController(model_name)
+        IKr_sim.set_protocol(times, voltages)
+        log = IKr_sim.simulate(log_var='all')
 
-    log.save_csv(os.path.join(data_dir, model_name + '_APclamp_current.csv'))
+        log.save_csv(os.path.join(
+            data_dir, f'{APmodel}_APclamp_{model_name}_current.csv'))
--- scripts/fig_scripts/base_hERGmodels.py
+++ scripts/fig_scripts/base_hERGmodels.py
@@ -1,40 +1,41 @@
+import matplotlib
+import matplotlib.pyplot as plt
 import myokit
 import os
 
 import modelling
 
-fig = modelling.figures.FigureStructure(figsize=(12, 6), gridspec=(2, 2),
-                                        height_ratios=[1, 2], hspace=0.3,
+fig = modelling.figures.FigureStructure(figsize=(12, 8), gridspec=(3, 2),
+                                        height_ratios=[1.5, 3, 4], hspace=0.51,
                                         wspace=0.3, plot_in_subgrid=True)
 plot = modelling.figures.FigurePlot()
 
-subgridspecs = [(3, 1), (3, 1)]
+ikr_gridspec = (2, 1)
+state_gridspec = [(2, 2), (2, 2)]
 subgs = []
+subgs.append(fig.gs[2:4].subgridspec(*ikr_gridspec, hspace=0.08,
+                                     height_ratios=[1, 1]))
 for i in range(2):
-    height_ratio = [1] * 2 + [2]
-    subgs.append(fig.gs[i + 2].subgridspec(*subgridspecs[i], wspace=0.1,
-                                           hspace=0.05,
-                                           height_ratios=height_ratio))
-axs = [[[fig.fig.add_subplot(subgs[k][i, j]) for j in range(
-    subgridspecs[k][1])] for i in range(subgridspecs[k][0])] for
-    k in range(len(subgs))]
+    subgs.append(fig.gs[i + 4].subgridspec(*state_gridspec[i], wspace=0.07,
+                                           hspace=0.08,
+                                           height_ratios=[1, 1]))
+ikr_axs = [fig.fig.add_subplot(subgs[0][i, 0]) for i in range(2)]
+state_axs = [[[fig.fig.add_subplot(subgs[k + 1][i, j]) for j in range(
+    state_gridspec[k][1])] for i in range(state_gridspec[k][0])] for
+    k in range(2)]
 
 data_dir = os.path.join(modelling.RESULT_DIR, 'background')
 fig_dir = os.path.join(modelling.FIG_DIR, 'background')
-
-model_list = ['Li', 'Lei']
-# Load steady state IKr data for drug free, addition of a dofetilide-like drug
-# and a verapamil-like drug conditions (AP clamp protocol)
-cipa_log = myokit.DataLog.load_csv(
-    os.path.join(data_dir, model_list[0] + '_APclamp_current.csv'))
-lei_log = myokit.DataLog.load_csv(
-    os.path.join(data_dir, model_list[1] + '_APclamp_current.csv'))
-log_all = [cipa_log, lei_log]
-pulse_time = 1000
+APmodels = modelling.model_naming.APmodel_list[:-1]
+AP_label = ['ORd-Li', 'Grandi', 'ten Tusscher', 'Tomek']
+current_keys = modelling.model_naming.model_current_keys
 
 # Plot state occupancies of the hERG channel
-color_seq = ['#ff0000', '#ff5555', '#999999', '#666666', '#333333',
-             '#808080', '#4d4d4d']
+# color_seq = ['#ff0000', '#ff5555', '#666666', '#999999', '#808080',
+#              '#333333', '#4d4d4d']
+color_seq = ['#00df00', '#50c850', '#666666', '#999999', '#808080',
+             '#333333', '#4d4d4d']
+model_color = plt.get_cmap('Dark2')
 
 # 80% gray: 333333
 # 70% gray: 4d4d4d
@@ -42,51 +43,93 @@ color_seq = ['#ff0000', '#ff5555', '#999999', '#666666', '#333333',
 # 50% gray: 808080
 # 40% gray: 999999
 
-panel_cipa = axs[0]
-panel_lei = axs[1]
-
-cipa_states = ['O', 'IO', 'IC1', 'C1', 'IC2', 'C2']
+li_states = ['O', 'IO', 'IC1', 'C1', 'IC2', 'C2']
 # state 'y3' is open state
 # lei_states = ['y3', 'y2', 'y1', 'y4']
 lei_states = ['O', 'I', 'CI', 'C']
 
-# Plot state occupancies of the hERG channel
-panel_cipa[2][0].stackplot(cipa_log.time(),
-                           *[cipa_log['ikr.' + s] for s in cipa_states],
-                           labels=[s for s in cipa_states],
-                           colors=color_seq[:6], zorder=-10,
-                           edgecolor='k')
-panel_cipa[2][0].legend(ncol=3, loc='upper right', handlelength=1,
-                        columnspacing=1, labelspacing=0.3)
-panel_cipa[2][0].set_ylim(bottom=0, top=1)
-panel_cipa[2][0].set_rasterization_zorder(0)
-panel_lei[2][0].stackplot(lei_log.time(),
-                          *[lei_log['ikr.' + s] for s in lei_states],
-                          labels=[s for s in lei_states],
-                          colors=color_seq[:4], zorder=-10,
-                          edgecolor='k')
-panel_lei[2][0].legend(ncol=2, loc='upper right', handlelength=1,
-                       columnspacing=1, labelspacing=0.3)
-panel_lei[2][0].set_ylim(bottom=0, top=1)
-panel_lei[2][0].set_rasterization_zorder(0)
+##################
+# Plot AP and IKr
+##################
+for m, APmodel in enumerate(APmodels):
+    # Plot AP-clamp protocol
+    prot_log = myokit.DataLog.load_csv(os.path.join(data_dir,
+                                                    f'APclamp_{APmodel}.csv'))
+    ikr_axs[0].plot(prot_log.time(), prot_log[current_keys[APmodel]['Vm']],
+                    color=model_color(m), label=APmodel)
 
-plot.add_single(panel_cipa[0][0], log_all[0], 'membrane.V')
-plot.add_single(panel_lei[0][0], log_all[1], 'membrane.V')
-plot.add_single(panel_cipa[1][0], log_all[0], 'ikr.IKr')
-plot.add_single(panel_lei[1][0], log_all[1], 'ikr.IKr')
+    m_row, m_col = int(m / 2), m % 2
 
+    # Plot IKr - Li model
+    li_log = myokit.DataLog.load_csv(os.path.join(
+        data_dir, f'{APmodel}_APclamp_Li_current.csv'))
+    ikr_axs[1].plot(li_log.time(), li_log['ikr.IKr'], color=model_color(m))
+
+    # Plot state occupancy - Li model
+    state_panel = state_axs[0][m_row][m_col]
+    state_panel.stackplot(
+        li_log.time(), *[li_log['ikr.' + s] for s in li_states],
+        labels=[s for s in li_states], colors=color_seq[:6], zorder=-10,
+        edgecolor='k')
+    state_panel.set_ylim(bottom=0, top=1)
+    state_panel.text(0.95, 0.9, AP_label[m], fontsize=8, ha='right', va='top',
+                     transform=state_panel.transAxes)
+    state_panel.set_rasterization_zorder(0)
+
+    # Plot IKr - Lei model
+    lei_log = myokit.DataLog.load_csv(os.path.join(
+        data_dir, f'{APmodel}_APclamp_Lei_current.csv'))
+    ikr_axs[1].plot(lei_log.time(), lei_log['ikr.IKr'], '--',
+                    color=model_color(m))
+
+    # Plot state occupancy - Lei model
+    state_panel = state_axs[1][m_row][m_col]
+    state_panel.stackplot(
+        lei_log.time(), *[lei_log['ikr.' + s] for s in lei_states],
+        labels=[s for s in lei_states], colors=color_seq[:4], zorder=-10,
+        edgecolor='k')
+    state_panel.set_ylim(bottom=0, top=1)
+    state_panel.text(0.95, 0.9, AP_label[m], fontsize=8, ha='right', va='top',
+                     transform=state_panel.transAxes)
+    state_panel.set_rasterization_zorder(0)
+
+state_axs[0][0][0].legend(ncol=6, loc='lower left', handlelength=1,
+                          bbox_to_anchor=(0, 1.0),
+                          columnspacing=1, labelspacing=0.3)
+state_axs[1][0][0].legend(ncol=4, loc='lower left', handlelength=1,
+                          bbox_to_anchor=(0, 1.0),
+                          columnspacing=1, labelspacing=0.3)
 # Adjust axes
-fig.sharex(['Time (ms)'], [(0, pulse_time)],
-           axs=panel_cipa, subgridspec=subgridspecs[0])
-fig.sharey(['Voltage\n(mV)', 'Current\n(A/F)', 'State\noccupancy'],
-           axs=panel_cipa, subgridspec=subgridspecs[0])
-fig.sharex(['Time (ms)'], [(0, pulse_time)],
-           axs=panel_lei, subgridspec=subgridspecs[1])
-fig.sharey(['Voltage\n(mV)', 'Current\n(A/F)', 'State\noccupancy'],
-           axs=panel_lei, subgridspec=subgridspecs[1])
+pulse_time = 1000
+ikr_axs[0].set_ylabel('Voltage \n (ms)')
+ikr_axs[1].set_ylabel('Current \n (A/F)')
+ikr_axs[0].sharex(ikr_axs[1])
+ikr_axs[0].tick_params(labelbottom=False)
+ikr_axs[1].set_xlabel('Time (ms)')
+ikr_axs[1].set_xlim((0, pulse_time))
+ikr_axs[0].set_box_aspect(0.15)
+ikr_axs[1].set_box_aspect(0.15)
+ikr_axs[0].spines[['right', 'top']].set_visible(False)
+ikr_axs[1].spines[['right', 'top']].set_visible(False)
 
-fig.fig.text(0.0855, 0.925, '(A)', fontsize=11)
-fig.fig.text(0.52, 0.925, '(B)', fontsize=11)
-fig.fig.text(0.0855, 0.56, '(C)', fontsize=11)
-fig.fig.text(0.52, 0.56, '(D)', fontsize=11)
-fig.savefig(os.path.join(fig_dir, "hERGmodels.svg"), format='svg')
+lines = []
+for i in range(4):
+    lines.append(matplotlib.lines.Line2D([0], [0], color=model_color(i), lw=5))
+lines.append(matplotlib.lines.Line2D([0], [0], color='k', linestyle='-'))
+lines.append(matplotlib.lines.Line2D([0], [0], color='k', linestyle='--'))
+
+ikr_axs[0].legend(lines, AP_label + ['Li', 'Lei'], loc='upper left',
+                  handlelength=1.5, bbox_to_anchor=(1.01, 0.5), ncol=1)
+
+for i in range(2):
+    fig.sharex(['Time (ms)'] * 2, [(0, pulse_time)] * 2,
+               axs=state_axs[i], subgridspec=state_gridspec[i])
+    fig.sharey(['State\noccupancy'] * 2,
+               axs=state_axs[i], subgridspec=state_gridspec[i])
+
+fig.fig.text(0.11, 0.925, '(A)', fontsize=11)
+fig.fig.text(0.56, 0.925, '(B)', fontsize=11)
+fig.fig.text(0.27, 0.7, '(C)', fontsize=11)
+fig.fig.text(0.09, 0.405, '(D)', fontsize=11)
+fig.fig.text(0.53, 0.405, '(E)', fontsize=11)
+fig.savefig(os.path.join(fig_dir, "hERGmodels_APs.svg"), format='svg')
